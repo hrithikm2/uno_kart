ef5a211cf55e508d96339322e94a1f4e700575aa
amigma00 <ali.aliaman786@gmail.com>
2021-05-05 14:44:34 +0530

    Deliverd order done

---
+++ lib/pages/components/List.dart
@@ -0,0 +1,14 @@
+List<dynamic> orderno = [
+  6711,
+  6587,
+  6548,
+  3214,
+  2547,
+];
+List<String> dish = [
+  "2 methi paratha",
+  "Chicken Biryani",
+  "Noodles",
+  "Manchurian",
+  "Masala Dosa",
+];
